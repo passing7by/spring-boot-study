1571b48c2dc0d52086f593b3bd952b3a1712fd76
passing7by <jinam0717@gmail.com>
2025-08-05 16:03:03 +0900

    ModelAndView를 사용하는 예시 추가

--- GDJ_92_Maven/src/main/java/com/winter/app/products/ProductController.java
+++ GDJ_92_Maven/src/main/java/com/winter/app/products/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/products/*")
@@ -77,8 +78,10 @@ public class ProductController {
 		return "products/product_form";
 	}
 	
+//	@PostMapping("update")
+//	public String update(ProductVO productVO, Model model, String str) throws Exception {
 	@PostMapping("update")
-	public String update(ProductVO productVO, Model model, String str) throws Exception {
+	public ModelAndView update(ProductVO productVO, Model model, String str) throws Exception {
 		int result = productService.update(productVO);
 		String url = "./list";
 		String msg = "수정 실패";
@@ -91,10 +94,19 @@ public class ProductController {
 			System.out.println("products/update - Post: 실패");
 		}
 		
-		model.addAttribute("url", url);
-		model.addAttribute("msg", msg);
+//		model.addAttribute("url", url);
+//		model.addAttribute("msg", msg);
+
+//		return "commons/result";
 		
-		return "commons/result";
+		// ModelAndView를 사용하는 방법
+		ModelAndView mv = new ModelAndView(); // 객체를 생성하지 않고 매개변수로 받아올 수도 있음
+		mv.addObject("msg", msg);
+		mv.addObject("url", "./list");
+		
+		mv.setViewName("commons/result");
+		
+		return mv;
 	}
 	
 	@PostMapping("delete")
